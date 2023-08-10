2067d5ec7ee43866ca10f234fba4a16e408c5934
nazrullayevkamoliddin <kamolxoja.n@gmail.com>
2023-08-10 12:13:02 +0500

    third commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -24,8 +24,8 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
-        <div className="relative w-full">
+      <Combobox value={manufacturer} onChange={setManufacturer}>
+        <div className="relative w-full">k
           <Combobox.Button className="absolute top-[14px]">
             <Image
               src="/car-logo.svg"
@@ -36,7 +36,7 @@ const SearchManufacturer = ({
             />
           </Combobox.Button>
           <Combobox.Input
-            className="search-manufacturer_input"
+            className="search-manufacturer__input"
             placeholder="Volkswagen"
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setQuery(e.target.value)}
@@ -49,7 +49,39 @@ const SearchManufacturer = ({
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            
+            <Combobox.Options>
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  value={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({selected, active}) => (
+                    <>
+                       <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
