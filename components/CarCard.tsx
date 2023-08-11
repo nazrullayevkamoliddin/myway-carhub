ab7b4eacf4a40015469aaac6a0694e6efd89569d
nazrullayevkamoliddin <kamolxoja.n@gmail.com>
2023-08-11 06:01:17 +0500

    CarCard - city_mpg, drive and transmission added

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -23,17 +23,53 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
 
       <p className="flex mt-6 text-[32px] font-extabold">
-        <span className="self-start text-[14px] font-semibold">  
-          $
-        </span>
+        <span className="self-start text-[14px] font-semibold">$</span>
         {carRent}
-        <span className="self-end  text-[14px] font-medium ">
-          /day
-        </span>
+        <span className="self-end  text-[14px] font-medium ">/day</span>
       </p>
 
       <div className="relative w-full h-40 my-3 object-contain">
-        <Image src='/hero.png' alt="car model" fill priority className="object-contain"/>
+        <Image
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      <div className="relative w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col jusfify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              alt="steering wheel"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">{transmission === 'a' ? 'Automatic' : "Manual"}</p>
+          </div>
+          <div className="flex flex-col jusfify-center items-center gap-2">
+            <Image
+              src="/tire.svg"
+              alt="tire"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col jusfify-center items-center gap-2">
+            <Image
+              src="/gas.svg"
+              alt="gas"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+
+        
       </div>
     </div>
   );
